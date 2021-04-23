1cc0df669211d004298c597aed2e8abfa817515c
svs <svs2504@gmail.com>
2021-04-23 08:46:59 +0530

    Add project files.

---
+++ MyFirstCoreWebApp/Data/MyFirstCoreWebAppContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using MyFirstCoreWebApp.Models;
+
+namespace MyFirstCoreWebApp.Data
+{
+    public class MyFirstCoreWebAppContext : DbContext
+    {
+        public MyFirstCoreWebAppContext (DbContextOptions<MyFirstCoreWebAppContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<MyFirstCoreWebApp.Models.Movie> Movie { get; set; }
+    }
+}
---
+++ MyFirstCoreWebApp/Pages/Movies/Index.cshtml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MyFirstCoreWebApp.Data;
+using MyFirstCoreWebApp.Models;
+
+namespace MyFirstCoreWebApp.Pages.Movies
+{
+    public class IndexModel : PageModel
+    {
+        private readonly MyFirstCoreWebApp.Data.MyFirstCoreWebAppContext _context;
+
+        public IndexModel(MyFirstCoreWebApp.Data.MyFirstCoreWebAppContext context)
+        {
+            _context = context;
+        }
+
+        public IList<Movie> Movie { get;set; }
+
+        public async Task OnGetAsync()
+        {
+            Movie = await _context.Movie.ToListAsync();
+        }
+    }
+}
